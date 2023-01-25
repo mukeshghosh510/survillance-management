3db8572ced54c67a9adbbc4ae9f21609558b888b
mukeshghosh510 <96496087+mukeshghosh510@users.noreply.github.com>
2023-01-25 12:01:51 +0530

    Update ground_truth_show.m

--- ground_truth_demo/ground_truth_demo/ground_truth_show.m
+++ ground_truth_demo/ground_truth_demo/ground_truth_show.m
@@ -1,10 +1,4 @@
-%   Distribution code Version 1.0 -- Oct 12, 2013 by Cewu Lu 
-%
-%   The Code can be used to show our grond truth in our Avenue Dataset, build based on  
-%   [1] "Abnormal Event Detection at 150 FPS in Matlab" , Cewu Lu, Jianping Shi, Jiaya Jia, 
-%   International Conference on Computer Vision, (ICCV), 2013
-%   
-%   The code and the algorithm are for non-comercial use only.
+
 
 videoIdx = 1; 
 
